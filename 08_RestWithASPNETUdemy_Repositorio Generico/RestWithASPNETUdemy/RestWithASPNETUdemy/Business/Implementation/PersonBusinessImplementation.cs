73a3d8c446fb678c49061f7b4dc067fdc59d6384
fabiosanromero <fabiosanromero@hotmail.com>
2022-01-09 17:18:16 -0300

    Utilizando Repositório Generico

---
+++ 08_RestWithASPNETUdemy_Repositorio Generico/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementation/PersonBusinessImplementation.cs
@@ -0,0 +1,41 @@
+﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Repository;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business.Implementation
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Person FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ 08_RestWithASPNETUdemy_Repositorio Generico/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -0,0 +1,96 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MySqlConnector;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Business.Implementation;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+using RestWithASPNETUdemy.Repository.Generic;
+using Serilog;
+using System;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            string mysqlConnectionstr = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(mysqlConnectionstr, ServerVersion.AutoDetect(mysqlConnectionstr)));
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(mysqlConnectionstr);
+            }
+
+            services.AddApiVersioning();
+
+            //Dependency Injections
+
+            services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        private void MigrateDatabase(string mysqlConnectionstr)
+        {
+            try
+            {
+                var evolveConnection = new MySqlConnection(mysqlConnectionstr);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations","db/dataset"},
+                    IsEraseDisabled=true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database Migration failed", ex);
+                throw;
+            }
+        }
+
+    }
+}
