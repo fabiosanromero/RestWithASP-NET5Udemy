9bff970cfac28f2f723523a043319b8606d4314e
fabiosanromero <fabiosanromero@hotmail.com>
2022-01-16 14:42:05 -0300

    Adicionando o suporte ao CORS
    Adicionando o Suporte ao CORS - Cross Origin Resourse Sharing, para permiter aos clientes da API realizar a chamada da sua API a partir de outro dominio.

---
+++ 12_RestWithASPNETUdemy_CORS/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Data.DTO;
+using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonDTO Create(PersonDTO person);
+        PersonDTO FindById(long id);
+        List<PersonDTO> FindAll();
+        PersonDTO Update(PersonDTO person);
+        void Delete(long id);
+    }
+}
