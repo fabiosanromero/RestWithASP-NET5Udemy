d6dd4b9efea32ad6c2072d18d56122ba79864f09
fabiosanromero <fabiosanromero@hotmail.com>
2022-01-16 17:39:00 -0300

    Autenticação Parte 1
    Implementando as Classes para geração e validação do Token de autenticação.

---
+++ 13_RestWithASPNETUdemy_Autenticação/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Data.DTO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using RestWithASPNETUdemy.Repository;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidateCredentials(UserDTO user)
+        {
+            var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+
+            return _context.Users.FirstOrDefault(u=>(u.UserName==user.UserName) && (u.Password==pass));
+        }
+
+        private string ComputeHash(string password, SHA256CryptoServiceProvider algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+
+        public User RefreshUserInfo(User user)
+        {
+            if (!_context.Users.Any(u => u.Id.Equals(user.Id))) return null;
+            
+            var result = _context.Users.SingleOrDefault(u => u.Id.Equals(user.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(user);
+                    _context.SaveChanges();
+                    return result;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return result;
+        }
+    }
+}
